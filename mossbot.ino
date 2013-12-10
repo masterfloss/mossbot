99c1cb84d1448b1522d5b49d57c456f5bfb668ec
mfvpt <marcovicente@yahoo.com>
2013-12-10 16:14:36 +0000

    Update mossbot.ino

--- mossbot.ino
+++ mossbot.ino
@@ -2,20 +2,25 @@
 // MOSSBot
 // 
 
+
+//TODO: criar classes próprias para motor e ultrasom
 //#include <motores.ino>
 //#include <ultrasom.ino>
 
 
-const int SafeDistance = 15;
+//Parâmetros do MOSSBot
+const int SafeDistance = 15; //segundos
 const int LoopCicleDelay = 20; //milisegundos
 
 bool turningLeft = false;
 bool turningRight = false;
 
+
 //MFV++ Configurações do MOSSBot
 void setup() 
 { 
   Serial.begin(9600);
+  //TODO: declarar portas de motores e ultrasom, aqui ou nas classes próprias
   //Declarar Portas dos Motores
   //pinMode(2, OUTPUT);
   //pinMode(3, INPUT);
@@ -35,14 +40,17 @@ int DecideBestDirection()
   
   //MFV++ distância de segurança 
   if(distance < SafeDistance)
-	{
-	  //valida se já está a virar para algum lado
+  {
+	//valida se já está a virar para algum lado
 	if (turningLeft)
+		//TODO: invocar motor vira à esquerda
 		//continua a virar à esquerda
 	else if (turningRight)
+		//TODO: invocar motor vira à direita
 		//continua a virar à direita
 	else
 	{
+		//TODO: invocar motor pára
 		//parar motor
 		 
 		//se tiver servo no futuro, poderemos validar se temos obstáculo à esquerda ou à direita e escolher
@@ -54,9 +62,10 @@ int DecideBestDirection()
 			turningRight = true;
 		    	turningLeft = false;
 		    	//Implementar classe motor
+		    	//TODO: invocar motor vira à direita
 			//motor.turnRight(50);
 		    	//digtialWrite(dir, HIGH);
-			Serial.println("Left");
+			Serial.println("Turning Right");
 		}
 		else
 		{
@@ -64,9 +73,10 @@ int DecideBestDirection()
 		    turningLeft = true;
 		    turningRight = false;
 		    //Implementar classe motor
-				//motor.turnLeft(50);
+		    //TODO: invocar motor vira à esquerda
+		    //motor.turnLeft(50);
 		    //digtialWrite(dir, LOW);
-				Serial.println("Left");
+	            Serial.println("Turning Left");
 		  }
 		}
 	}
@@ -83,6 +93,8 @@ int DecideBestDirection()
 
 
 //MFV++ Deverá passar a classe própria
+//TODO: criar classe própria e implementar função
+//pode ser aproveitado o código do ultrasom.ino
 int SonnarGetDistance()
 {
 
