c7028f8f9b20e7d2cf34da3746ea6121a41d9e9b
mfvpt <marcovicente@yahoo.com>
2013-12-10 15:49:33 +0000

    Update mossbot.ino

--- mossbot.ino
+++ mossbot.ino
@@ -2,7 +2,12 @@
 // MOSSBot
 // 
 
+//#include <motores.ino>
+//#include <ultrasom.ino>
+
+
 const int SafeDistance = 15;
+const int LoopCicle = 20; //milisegundos
 
 bool turningLeft = false;
 bool turningRight = false;
@@ -10,10 +15,13 @@ bool turningRight = false;
 //MFV++ Configurações do MOSSBot
 void setup() 
 { 
-	
+  Serial.begin(9600);
+  //Declarar Portas dos Motores
+  pinMode(2, OUTPUT);
+  pinMode(3, INPUT);
+  pinMode(4, OUTPUT);pinMode(5, OUTPUT);     
+  pinMode(6, OUTPUT);pinMode(7, OUTPUT);  	
 } 
-
-
 
 //MFV++ Deverá passar a classe própria
 int DecideBestDirection()
@@ -29,29 +37,29 @@ int DecideBestDirection()
   if(distance < SafeDistance)
 	{
 	  //valida se já está a virar para algum lado
-		if (turningLeft)
-			//continua a virar à esquerda
-		else if (turningRight)
-			//continua a virar à direita
+	if (turningLeft)
+		//continua a virar à esquerda
+	else if (turningRight)
+		//continua a virar à direita
+	else
+	{
+		//parar motor
+		 
+		//se tiver servo no futuro, poderemos validar se temos obstáculo à esquerda ou à direita e escolher
+		//para já vira para um lado qualquer
+		 
+		if(random(100) > 50)
+		{
+			//vira à direita
+			turningRight = true;
+		    	turningLeft = false;
+		    	//Implementar classe motor
+			//motor.turnRight(50);
+		    	//digtialWrite(dir, HIGH);
+			Serial.println("Left");
+		}
 		else
 		{
-		  //parar motor
-		  
-		  //se tiver servo no futuro, poderemos validar se temos obstáculo à esquerda ou à direita e escolher
-		  //para já vira para um lado qualquer
-		  
-		  if(random(100) > 50)
-		  {
-		  	//vira à direita
-		    turningRight = true;
-		    turningLeft = false;
-		    //Implementar classe motor
-				//motor.turnRight(50);
-		    //digtialWrite(dir, HIGH);
-				Serial.println("Left");
-		  }
-		  else
-		  {
 		    //vira à esquerda
 		    turningLeft = true;
 		    turningRight = false;
